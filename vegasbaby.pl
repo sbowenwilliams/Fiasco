712b3544cc7d2ac0f317a328d53382c2d2561807
Sean Bowen-Williams <spbowenwilliams@gmail.com>
2015-04-20 20:36:00 -0500

    Added more relationships

--- vegasbaby.pl
+++ vegasbaby.pl
@@ -1,12 +1,35 @@
+:- export((need/1, relationship/3, location/1, object/1,
+	   relation/1, roles_relation/1, symmetric/1,
+	   implies/2, conflicting_roles/2, generalization/2,
+	   contradiction/2)).
+
 relation(sibling).
 relation(parent).
+relation(child).
 relation(lover).
 relation(killer).
 relation(victim).
-relation(leader).
+relation(rival).
 relation(fling).
-relation(associates).
+relation(associate).
+relation(competitor).
+relation(friends).
+relation(employer).
+relation(employee).
+relation(leader).
+relation(thug).
+
 
 symmetric(sibling).
 symmetric(associates).
+symmetric(rival).
+symmetric(competitor).
+symmetric(fiends).
+symmetric(fling).
+
+roles_relation(parent/child).
+roles_relation(killer/victim).
+roles_relation(leader/thug).
+roles_relation(employer/employee).
+
 
