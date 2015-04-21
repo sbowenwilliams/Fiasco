882af4960904815a430ac1ee23c81175daaf706e
Sean Bowen-Williams <spbowenwilliams@gmail.com>
2015-04-20 19:30:44 -0500

    Initial

---
+++ vegasbaby.pl
@@ -0,0 +1,12 @@
+relation(sibling).
+relation(parent).
+relation(lover).
+relation(killer).
+relation(victim).
+relation(leader).
+relation(fling).
+relation(associates).
+
+symmetric(sibling).
+symmetric(associates).
+
